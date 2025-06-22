cc3a7b2a56a601380388274235cac6f1b063c831
Roberto Victor <robertovictor_rj@hotmail.com>
2025-06-22 09:49:06 -0300

    Add workflow config template.

--- terraform/dataform.tf
+++ terraform/dataform.tf
@@ -23,3 +23,34 @@ resource "google_dataform_repository_release_config" "release" {
     }
   }
 }
+
+resource "google_dataform_repository_workflow_config" "workflow" {
+  provider = google-beta
+
+  project        = google_dataform_repository.repository.project
+  region         = google_dataform_repository.repository.region
+  repository     = google_dataform_repository.repository.name
+  name           = "my_workflow"
+  release_config = google_dataform_repository_release_config.release_config.id
+
+  invocation_config {
+    included_targets {
+      database = "gcp-example-project"
+      schema   = "example-dataset"
+      name     = "target_1"
+    }
+    included_targets {
+      database = "gcp-example-project"
+      schema   = "example-dataset"
+      name     = "target_2"
+    }
+    included_tags                            = ["tag_1"]
+    transitive_dependencies_included         = true
+    transitive_dependents_included           = true
+    fully_refresh_incremental_tables_enabled = false
+    service_account                          = google_service_account.dataform_sa.email
+  }
+
+  cron_schedule   = "0 7 * * *"
+  time_zone       = "America/New_York"
+}
