fb82eda931baf0f809b2191c9086bac4965ebf2e
Roberto Victor Silva Bezerra <roberto.bezerra@g.globo>
2025-06-22 21:32:18 -0300

    Add templates for sqlx.

--- terraform/dataform.tf
+++ terraform/dataform.tf
@@ -1,3 +1,22 @@
+ resource "google_dataform_repository" "dataform_repository" {
+   project = var.projeto-pipeline
+   provider = google-beta
+   name = "${var.solucao}${local.sufixo}"
+   region = var.regiao
+   timeouts {}
+
+   git_remote_settings {
+       url = var.git-repository
+       default_branch = var.branch
+       authentication_token_secret_version = "projects/${var.projeto-pipeline}/secrets/${var.solucao}/versions/latest"
+   }
+
+   workspace_compilation_overrides {
+     schema_suffix = var.branch
+     default_database = var.projeto-pipeline
+   }
+ }
+
 resource "google_dataform_repository_release_config" "release" {
   provider = google-beta
 
